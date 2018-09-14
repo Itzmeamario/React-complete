4194208032925b6bbfeea0cb8b942fbf1562b86a
itzmeamario <mario.santamaria.portilla@gmail.com>
2018-09-13 17:27:47 -0700

    added styled components and modified the webpack for compatability

--- src/Person/Person.js
+++ src/Person/Person.js
@@ -1,10 +1,10 @@
 import React from 'react';
-import './Person.css';
+import classes from './Person.css';
 
 const person = ({name, age, click, changed, children}) => {
 
   return (
-    <div className="Person">
+    <div className={classes.Person}>
       <p onClick={click}>I'm {name} and I am {age} years old!</p>
       <p>{children}</p>
       <input type="text" onChange={changed} value={name} />
